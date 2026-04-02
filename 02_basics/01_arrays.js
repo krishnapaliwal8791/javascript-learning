98cf61d50042c4dccad1c1b9cff8c2af2323c7e4
krishnapaliwal8791 <krishna.p.8791@gmail.com>
2026-04-02 12:45:26 +0530

    Completed Arrays : 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,57 @@
+// Arrays in JS
+const arr = ["Krishna", true, 10, 12n]
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// JS arrays are resizable
+// When we do copy operation on arrays it will create shallow copy 
+
+// Declaring array using new keyword
+const arr3 = new Array(1,2,3,4,5,6)
+
+let myarr = new Array
+// Array Methods
+// 1. push()
+myarr.push(10)
+console.table(myarr)
+myarr.push(11)
+console.table(myarr)
+
+// 2. pop()
+myarr.pop()
+console.table(myarr)
+
+// 3. unshift()
+myarr.unshift(9)
+console.table(myarr)
+
+// 4. shift()
+myarr.shift()
+console.table(myarr)
+
+// 5. includes()
+console.log(myarr.includes(9));
+console.log(myarr.includes(10));
+
+// 6. indexOf()
+console.log(myarr.indexOf(9));
+console.log(myarr.indexOf(10));
+myarr = [1,2,3,3,4,3]
+console.log(myarr.indexOf(3));
+
+// 7. join()
+myarr = [1,2,3,4,5]
+const newarr = myarr.join()
+console.log(`myarr -> `,myarr)
+console.log(`ntypeof myarr -> ${typeof myarr}\nnewarr -> ${newarr} \ntypeof newarr -> ${typeof newarr}`);
+
+// 8. slice() and splice()
+myarr = [0,1,2,3,4,5]
+console.log("A : ", myarr);
+let slice_arr = myarr.slice(1,3)
+console.log("slice_arr : ", slice_arr);
+console.log("B : ", myarr);
+let splice_arr = myarr.splice(1,3)
+console.log("splice_arr : ", splice_arr);
+console.log("C : ", myarr);
+
+// Splice includes the last element in the result while slice doesnot includes last element in the result
+// Splice affects the original array while slice doesnot affect the original one
