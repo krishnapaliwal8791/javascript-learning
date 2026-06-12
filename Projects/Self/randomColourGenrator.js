48f8cea7c451bf35c8818f89b0285725c4e51e38
krishnapaliwal8791 <krishna.p.8791@gmail.com>
2026-06-12 15:37:07 +0530

    Random Colour Genrator added

---
+++ Projects/Self/randomColourGenrator.js
@@ -0,0 +1,7 @@
+let r,g,b,alpha
+r = Math.floor(Math.random()*255)
+g = Math.floor(Math.random()*255)
+b = Math.floor(Math.random()*255)
+alpha = Math.random()
+alpha = alpha.toFixed(2)
+console.log(`The Colour is (${r},${g},${b}) with transperency ${alpha}`);
