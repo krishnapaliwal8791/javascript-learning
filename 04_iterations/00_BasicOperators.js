0e284da5eee0b7e2e6f2d6d029c66cca9a7d4389
krishnapaliwal8791 <krishna.p.8791@gmail.com>
2026-06-12 16:09:05 +0530

    ..

---
+++ 04_iterations/00_BasicOperators.js
@@ -0,0 +1,14 @@
+// Nullish Coalescing Operator ( ?? ): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = undefined ?? 15 
+
+console. log(val1) ;
+
+// Ternary Operator
+// condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice >=80 ? console.log(`Price >= 80`) : console.log(`Price < 80`);
+
